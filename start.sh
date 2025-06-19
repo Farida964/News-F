c0105abc2905b5b47b1de6e6bf905f36135a8a67
Farida964 <imfaravi@gmail.com>
2025-06-19 21:16:13 +0700

    cc

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
+php artisan config:cache
+php artisan migrate --force
+
 php artisan serve --host=0.0.0.0 --port=$PORT
